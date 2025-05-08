a7efd44836712c2c66c5897e9ff79df18f7a733a
Vishal Gupta <vishalgupta7972@gmail.com>
2025-05-09 00:45:03 +0200

    x

--- include/linux/mmap_lock.h
+++ include/linux/mmap_lock.h
@@ -97,7 +97,8 @@ static inline void vma_end_write_all(struct mm_struct *mm) {}
 
 static inline void mmap_init_lock(struct mm_struct *mm)
 {
-	init_rwsem_static_tclock(&mm->mmap_lock);
+	init_rwsem(&mm->mmap_lock);
+//	init_rwsem_static_tclock(&mm->mmap_lock);
 //	init_rwsem_static_percpu(&mm->mmap_lock);
 }
 
