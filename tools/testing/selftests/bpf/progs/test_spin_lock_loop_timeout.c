bac78c96f4093dc96060dfc0d06325e9d7bf73d7
Vishal Gupta <vishalgupta7972@gmail.com>
2026-01-27 17:10:47 +0000

    Update test cases

--- tools/testing/selftests/bpf/progs/test_spin_lock_loop_timeout.c
+++ tools/testing/selftests/bpf/progs/test_spin_lock_loop_timeout.c
@@ -11,7 +11,7 @@
  */
 #include <vmlinux.h>
 #include <bpf/bpf_helpers.h>
-#include "bpf_misc.h"
+//#include "bpf_misc.h"
 
 struct lock_data {
 	struct bpf_spin_lock lock;
@@ -25,14 +25,26 @@ struct {
 	__type(value, struct lock_data);
 } lock_map SEC(".maps");
 
-static long loop_callback(u32 index, void *ctx)
-{
-	volatile int *counter = ctx;
+int pid;
 
-	/* Do some work each iteration */
-	(*counter)++;
+#define LOOPS_CNT 1 << 10
 
-	/* Never return 1, so loop continues until timeout or nr_loops */
+static int callback_fn4(void *ctx) {
+	return 0;
+}
+
+static int callback_fn3(void *ctx) {
+	bpf_loop(LOOPS_CNT, callback_fn4, NULL, 0);
+	return 0;
+}
+
+static int callback_fn2(void *ctx) {
+	bpf_loop(LOOPS_CNT, callback_fn3, NULL, 0);
+	return 0;
+}
+
+static int callback_fn(void *ctx) {
+	bpf_loop(LOOPS_CNT, callback_fn2, NULL, 0);
 	return 0;
 }
 
@@ -40,21 +52,17 @@ static long loop_callback(u32 index, void *ctx)
  * Test: Acquire spinlock and run a very long bpf_loop.
  * The spinlock timeout should trigger and terminate the program
  * before the loop completes naturally.
- *
- * Expected behavior:
- * - Lock is acquired
- * - Loop starts running
- * - hrtimer fires after sysctl_bpf_spin_lock_timeout ms
- * - ebpf_spinlock_timeout flag is set
- * - bpf_loop detects flag and calls timeout handler
- * - Lock is released and program is terminated
  */
+//SEC("tp/syscalls/sys_enter_socket")
 SEC("tc")
 int test_spinlock_loop_timeout(struct __sk_buff *ctx)
 {
 	int key = 0;
 	struct lock_data *data;
 	volatile int counter = 0;
+
+	if ((bpf_get_current_pid_tgid() >> 32) != pid)
+		return 0;
 
 	data = bpf_map_lookup_elem(&lock_map, &key);
 	if (!data)
@@ -65,19 +73,12 @@ int test_spinlock_loop_timeout(struct __sk_buff *ctx)
 
 	bpf_printk("Lock acquired, starting long loop\n");
 
-	/*
-	 * Run a very large number of iterations.
-	 * With the timeout mechanism, this should be interrupted
-	 * before completing all iterations.
-	 * Using 0xFFFFFFFF (max u32) to ensure we hit the timeout.
-	 */
-	bpf_loop(0xFFFFFFFF, loop_callback, (void *)&counter, 0);
+	counter++;
+	bpf_loop(LOOPS_CNT, callback_fn, NULL, 0);
+	counter++;
 
-	/*
-	 * If we reach here without the timeout firing, unlock normally.
-	 * In the expected timeout case, bpf_die() is called and we
-	 * never reach this point.
-	 */
+	bpf_printk("This should not be reached: %d\n", counter);
+
 	bpf_spin_unlock(&data->lock);
 
 	bpf_printk("Loop completed with counter: %d\n", counter);
