9a22cbbaa66fe5d73d410de35b57569fbfaff93d
Vishal Gupta <vishalgupta7972@gmail.com>
2026-01-30 15:38:58 +0000

    update loop timeout testing code

--- tools/testing/selftests/bpf/prog_tests/spin_lock_loop_timeout.c
+++ tools/testing/selftests/bpf/prog_tests/spin_lock_loop_timeout.c
@@ -6,14 +6,65 @@
 
 #include "test_spin_lock_loop_timeout.skel.h"
 
+#define SYSCTL_PATH "/proc/sys/net/core/bpf_spin_lock_timeout"
+
+static int __read_sysctl(void)
+{
+	int fd, val = 0;
+	char buf[32];
+
+	fd = open(SYSCTL_PATH, O_RDONLY);
+	if (fd < 0)
+		return -1;
+
+	if (read(fd, buf, sizeof(buf)) > 0)
+		val = atoi(buf);
+
+	close(fd);
+	return val;
+}
+
+static int __write_sysctl(int val)
+{
+	int fd;
+	char buf[32];
+
+	fd = open(SYSCTL_PATH, O_WRONLY);
+	if (fd < 0)
+		return -1;
+
+	snprintf(buf, sizeof(buf), "%d\n", val);
+	if (write(fd, buf, strlen(buf)) < 0) {
+		close(fd);
+		return -1;
+	}
+
+	close(fd);
+	return 0;
+}
+
+
+
 static void trigger_spinlock_loop_timeout(void)
 {
 	struct test_spin_lock_loop_timeout *skel;
-	int prog_fd, err;
+	int prog_fd, err, old_timeout;
+
+	/* Save current timeout and set a short timeout */
+	old_timeout = __read_sysctl();
+	if (old_timeout < 0) {
+		test__skip();
+		return;
+	}
+
+	if (__write_sysctl(500) < 0) {  /* 500ms timeout */
+		test__skip();
+		return;
+	}
 
 	skel = test_spin_lock_loop_timeout__open();
 	if (!ASSERT_OK_PTR(skel, "test_spin_lock_loop_timeout__open"))
-	        return;
+	        goto cleanup;
 	
 	err = test_spin_lock_loop_timeout__load(skel);
 	if (!ASSERT_OK(err, "test_spin_lock_loop_timeout__load"))
@@ -39,6 +90,8 @@ static void trigger_spinlock_loop_timeout(void)
 	ASSERT_TRUE(true, "Program is terminated");
 out:
        test_spin_lock_loop_timeout__destroy(skel);
+cleanup:
+	__write_sysctl(old_timeout);
 }
 
 void test_spin_lock_loop_timeout(void)
