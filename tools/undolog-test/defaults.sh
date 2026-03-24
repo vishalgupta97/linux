313403f04325130a19a3cf2fa24ae8e0b88fe7a6
Vishal Gupta <vishalgupta7972@gmail.com>
2026-03-24 16:40:03 +0000

    srv10 baseline numbers

--- tools/undolog-test/defaults.sh
+++ tools/undolog-test/defaults.sh
@@ -1,15 +1,15 @@
 
-export cores=(1 2 4 8 14 28 42 56 112)
-export ncores=112
-export root_dir=`pwd`
-export results_dir=${root_dir}/doc/results/srv1vm
-export runtime=10
-export kernel=`uname -r | sed 's/5.14.16-//'`
-
-#export cores=(1 2 4 8 12 24 48 60 72 96)
-#export ncores=96
+#export cores=(1 2 4 8 14 28 42 56 112)
+#export ncores=112
 #export root_dir=`pwd`
-#export results_dir=${root_dir}/doc/results/srv10vm
+#export results_dir=${root_dir}/doc/results/srv1vm
 #export runtime=10
 #export kernel=`uname -r | sed 's/5.14.16-//'`
+
+export cores=(1 2 4 8 12 24 36 48 60 72 84 96)
+export ncores=96
+export root_dir=`pwd`
+export results_dir=${root_dir}/doc/results/srv10vm
+export runtime=10
+export kernel=`uname -r | sed 's/5.14.16-//'`
 
