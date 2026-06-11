cf06f34e178624b254d89220115d0d8e9eda1ec6
Vishal Gupta <vishalgupta7972@gmail.com>
2026-06-11 12:59:35 +0000

    fpop results

--- tools/testing/selftests/bpf/benchs/run_bench_spinlock.sh
+++ tools/testing/selftests/bpf/benchs/run_bench_spinlock.sh
@@ -67,11 +67,11 @@ run_bench() {
 }
 
 {
-	for thread in 1 2 4 8 16 32 64 80 96 112 128; do
+	for thread in 1 2 4 8 16 28 32 64 80 96 112; do
 		for pool in 128; do #1 8 32 128; do
-			for ds in ring list trie graph; do
+			for ds in list; do #ring trie graph 
 			for op in insert; do  #lookup update delete; do
-				for variant in kmod kmod_bpf undo_log; do
+				for variant in lock_func; do #kmod kmod_bpf undo_log; do
 				
 				# For insert, no prefill; for read/write/delete ops
 				# prefill to full pool capacity.
